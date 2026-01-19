680391121463e4d515e6796623529fd2bbebd5c5
sy3d-m3hd1 <tatheermehdi85@gmail.com>
2026-01-19 18:52:04 +1100

    py

---
+++ afterclass.py
@@ -0,0 +1,14 @@
+username = input("What's your name: ")
+print(f"Nice to meet you, {username}3nigg")
+
+age = input("How old are you: ")
+print(f"Wow, {age} years old")
+
+ethnicity = input("What's your ethnicity: ")
+print(f"Cool, {ethnicity}")
+
+hobby = input("What's your favourite hobby: ")
+print(f"Nice {hobby} sounds fun")
+
+city = input("Which city are you from: ")
+print(f"lovely, {username} from {city}")
